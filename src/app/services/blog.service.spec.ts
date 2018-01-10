5e15188a16e35c9d9f9693a6feb5899bc68da158
Pablo Anglat <panglat@gotocme.com>
2018-01-10 17:47:10 -0300

    Fix blog.service.spec.ts error

--- src/app/services/blog.service.spec.ts
+++ src/app/services/blog.service.spec.ts
@@ -1,15 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { BlogService } from './blog.service';
 
 describe('BlogService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [BlogService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [BlogService]
+        });
     });
-  });
 
-  it('should be created', inject([BlogService], (service: BlogService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([BlogService], (service: BlogService) => {
+        expect(service).toBeTruthy();
+    }));
 });
